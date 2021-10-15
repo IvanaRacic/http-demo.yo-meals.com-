6600413b396ee7f7463e27b71f52690f8c00db63
IvanaRacic <ivana.m.racic@gmail.com>
2021-10-15 13:52:06 +0200

    AfterMethod

--- src/tests/BasicTest.java
+++ src/tests/BasicTest.java
@@ -1,12 +1,21 @@
 package tests;
 
+import java.io.File;
+import java.io.IOException;
+import java.sql.Timestamp;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 import pages.AuthPage;
@@ -58,6 +67,21 @@ public abstract class BasicTest {
 		cartSummaryPage = new CartSummaryPage (driver, waiter, js);
 		mealPage = new MealPage (driver, waiter, js);
 		
+	}
+	
+	@AfterMethod
+	
+	public void afterMethod () throws IOException {
+		TakesScreenshot scrShot =((TakesScreenshot)driver);
+		File SrcFile=scrShot.getScreenshotAs(OutputType.FILE);
+        Instant instant = Instant.now().truncatedTo( ChronoUnit.SECONDS );
+		String fileName = instant.toString().replace(":", "-");
+		File DestFile=new File("screenshots/" + fileName + ".png");
+		FileUtils.copyFile(SrcFile, DestFile);
+		
+		driver.quit();
+		
+	
 	}
 	
 }
